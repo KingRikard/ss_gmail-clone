e83c687afe2017191909225a1306a3f37dda003a
KingRikard <rikard.krvaric@gmail.com>
2021-05-31 10:25:36 -0500

    GmailClone_Initial

---
+++ src/EmailList.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Checkbox, IconButton } from "@material-ui/core";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+import "./EmailList.css";
+
+
+function EmailList() {
+    return (
+        <div className="emailList">
+            <div className="emailList__settings">
+                <div className="emailList__settingsLeft">
+                    <Checkbox />
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+                <div className="emailList__settingsRight">
+                    <IconButton>
+                        <KeyboardArrowLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardArrowRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="emailList__sections">
+
+            </div>
+        </div>
+    )
+}
+
+export default EmailList
---
+++ src/Mail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mail() {
+    return (
+        <div className="mail">
+            MAIL
+        </div>
+    )
+}
+
+export default Mail
